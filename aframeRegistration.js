9785e5781612eb68ec8eb866e0cb78462409fdef
Vivian Liu <vliu3@logitech.com>
2019-08-13 09:56:40 -0700

    updating custommarkers

---
+++ aframeRegistration.js
@@ -0,0 +1,65 @@
+
+THREEx.ArToolkitContext.baseURL = 'https://rawgit.com/jeromeetienne/ar.js/master/three.js/';
+
+AFRAME.registerComponent('trigger', {
+
+    init: function () {
+        document.addEventListener('keydown', evt => {
+            if (evt.keyCode === 32) {
+                this.trigger();
+            }
+        });
+        document.addEventListener('click', () => {
+            this.trigger();
+        });
+    },
+
+    trigger: function () {
+        this.el.emit('particleplayerstart', {
+            position: new THREE.Vector3(
+                (Math.random() - 0.5) * 7,
+                Math.random() * 2,
+                -5 - Math.random() * 2
+            ),
+            rotation: new THREE.Euler(Math.random() * 1 - .5, 0, 0)
+        });
+    }
+
+
+});
+
+
+AFRAME.registerComponent('registerevents', {
+    init: function () {
+        var marker = this.el;
+
+        marker.addEventListener('markerFound', function () {
+            var markerId = marker.id;
+            console.log('markerFound', markerId);
+            text = marker.children[0];
+
+            document.getElementById("infoImg").src = text.getAttribute('data-infoImgSrc');
+
+            document.getElementById("testtext").innerText = text.getAttribute('data-textValue');
+
+            // if (markerId == 0) {
+            //     document.getElementById("map").style.display = 'block';
+            //     document.getElementById('modal').style.display = 'none';
+            //
+            // }
+
+
+
+        });
+        marker.addEventListener('markerLost', function () {
+            document.getElementById("map").style.display = 'none';
+        });
+
+    }
+});
+
+
+function directTo(index) {
+    var urlArray = ['https://support.logi.com/hc/en-us/articles/360025337813-FAQ-G910-Orion-Spectrum-RGB-Mechanical-Gaming-Keyboard','https://support.logi.com/hc/en-us/articles/360025407993-Spare-Parts-G910-Orion-Spectrum-RGB-Mechanical-Gaming-Keyboard', 'https://www.logitech.com/en-us/sustainability/recycling.html'];
+    window.open(urlArray[index]);
+}
---
+++ initializeMap.js
@@ -0,0 +1,136 @@
+var map;
+var service;
+var infowindow;
+var lat = 37.539;
+var long = -122.062;
+
+
+function initMap() {
+    var start = new google.maps.LatLng(lat, long);
+
+    infowindow = new google.maps.InfoWindow();
+
+    map = new google.maps.Map(
+        document.getElementById('map'), {center: start, zoom: 12,
+            styles: [
+                {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
+                {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
+                {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
+                {
+                    featureType: 'administrative.locality',
+                    elementType: 'labels.text.fill',
+                    stylers: [{color: '#d59563'}]
+                },
+                {
+                    featureType: 'poi',
+                    elementType: 'labels.text.fill',
+                    stylers: [{color: '#d59563'}]
+                },
+                {
+                    featureType: 'poi.park',
+                    elementType: 'geometry',
+                    stylers: [{color: '#263c3f'}]
+                },
+                {
+                    featureType: 'poi.park',
+                    elementType: 'labels.text.fill',
+                    stylers: [{color: '#6b9a76'}]
+                },
+                {
+                    featureType: 'road',
+                    elementType: 'geometry',
+                    stylers: [{color: '#38414e'}]
+                },
+                {
+                    featureType: 'road',
+                    elementType: 'geometry.stroke',
+                    stylers: [{color: '#212a37'}]
+                },
+                {
+                    featureType: 'road',
+                    elementType: 'labels.text.fill',
+                    stylers: [{color: '#9ca5b3'}]
+                },
+                {
+                    featureType: 'road.highway',
+                    elementType: 'geometry',
+                    stylers: [{color: '#746855'}]
+                },
+                {
+                    featureType: 'road.highway',
+                    elementType: 'geometry.stroke',
+                    stylers: [{color: '#1f2835'}]
+                },
+                {
+                    featureType: 'road.highway',
+                    elementType: 'labels.text.fill',
+                    stylers: [{color: '#f3d19c'}]
+                },
+                {
+                    featureType: 'transit',
+                    elementType: 'geometry',
+                    stylers: [{color: '#2f3948'}]
+                },
+                {
+                    featureType: 'transit.station',
+                    elementType: 'labels.text.fill',
+                    stylers: [{color: '#d59563'}]
+                },
+                {
+                    featureType: 'water',
+                    elementType: 'geometry',
+                    stylers: [{color: '#17263c'}]
+                },
+                {
+                    featureType: 'water',
+                    elementType: 'labels.text.fill',
+                    stylers: [{color: '#515c6d'}]
+                },
+                {
+                    featureType: 'water',
+                    elementType: 'labels.text.stroke',
+                    stylers: [{color: '#17263c'}]
+                }
+            ]
+
+        });
+
+    var request = {
+        location: start,
+        radius: 10000,
+        keyword: "recycle"
+    };
+
+    service = new google.maps.places.PlacesService(map);
+
+    service.nearbySearch(request, function(results, status) {
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
+            for (var i = 0; i < results.length; i++) {
+                console.log(results[i])
+                createMarker(results[i]);
+            }
+
+            map.setCenter(results[0].geometry.location);
+        }
+    });
+}
+
+function createMarker(place) {
+    var marker = new google.maps.Marker({
+        map: map,
+        position: place.geometry.location
+    });
+
+    google.maps.event.addListener(marker, 'click', function() {
+        infowindow.setContent(place.name);
+        infowindow.open(map, this);
+    });
+}
+
+function regenerateMat() {
+    lat = document.getElementById("enterLat").value;
+    console.log(lat);
+    long = document.getElementById("enterLong").value;
+    initMap();
+
+}
