d34d54fe63276bfcd2cd3c6bb13ce2e72ef48883
Vivian Liu <vliu3@logitech.com>
2019-08-19 09:18:44 -0700

    updated sustainability for demo

--- aframeRegistration.js
+++ aframeRegistration.js
@@ -48,50 +48,15 @@ AFRAME.registerComponent('changegif', {
     }
 });
 
-AFRAME.registerComponent('autoscale', {schema: {type: 'number', default: 1},
-    init: function () {
-        this.scale();this.el.addEventListener('object3dset', () => this.scale());},scale: function () {
-        const el = this.el;
-        const span = this.data;
-        const mesh = el.getObject3D('mesh');
-        if (!mesh) return;
-        const bbox = new THREE.Box3().setFromObject(mesh);
-        const scale = span / bbox.getSize().length();
-        var sx = this.el.getAttribute('scale').x;
-        var sy = this.el.getAttribute('scale').y;
-        var sz = this.el.getAttribute('scale').z;
-
-        var rx = this.el.getAttribute('rotation').x * (Math.PI / 180);
-        var ry = this.el.getAttribute('rotation').y * (Math.PI / 180);
-        var rz = this.el.getAttribute('rotation').z * (Math.PI / 180);
-        mesh.rotation.set(rx,ry,rz);
-        mesh.scale.set(scale*sx, scale*sy, scale*sz);
-
-        var a = new THREE.Box3().setFromObject(this.el.object3D);
-        var cx = (a.min.x + a.max.x)/2;
-        var cy = (a.min.y + a.max.y)/2;
-        var cz = (a.min.z + a.max.z)/2;
-        var posx = this.el.object3D.position.x;
-        var posy = this.el.object3D.position.y;
-        var posz = this.el.object3D.position.z;
-        console.log("boundingBox xyz: x: "+cx+", y: "+cy+" z: "+cz);
-        console.log("box position xyz: x: "+posx+", y: "+posy+" z: "+posz);
-        var translateX = posx - cx;
-        var translateY = posy - cy;
-        var translateZ = posz - cz;
-        this.el.object3DMap.mesh.translateX(translateX/sx);
-        this.el.object3DMap.mesh.translateY(translateY/sy);
-        this.el.object3DMap.mesh.translateZ(translateZ/sz);
-    }
-});
 
 AFRAME.registerComponent('registerevents', {
     init: function () {
         var marker = this.el;
 
         marker.addEventListener('markerFound', function () {
-            var markerId = marker.id;
+            var markerId = marker.getAttribute('data-name');
             console.log('markerFound', markerId);
+
             text = marker.children[0];
             console.log(text.getAttribute('data-infoImgSrc'));
 
@@ -99,7 +64,9 @@ AFRAME.registerComponent('registerevents', {
 
             document.getElementById("testtext").innerText = text.getAttribute('data-textValue');
 
-            if (markerId == 2) {
+
+            if (markerId == 'recycle') {
+                console.log("changing style");
                 document.getElementById("map").style.display = 'block';
                 document.getElementById("modal").style.display = 'none';
 
@@ -115,6 +82,8 @@ AFRAME.registerComponent('registerevents', {
 
     }
 });
+
+
 
 
 function directTo(index) {
